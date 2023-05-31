a282bb05b4afac46ff5f390d9ed6fcd8c8614884
Daniil_Shakhmovedev <dshahmovedeev@intetech.ru>
2023-05-31 16:17:46 +0300

    init v3

---
+++ ms-auth/src/main/java/ru/profind/msauth/MsAuthApplication.java
@@ -0,0 +1,15 @@
+package ru.profind.msauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MsAuthApplication.class, args);
+    }
+
+}
