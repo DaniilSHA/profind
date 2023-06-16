f28a962edc015e32fbad94c242ef6a642508d3b2
Daniil_Shakhmovedev <dshahmovedeev@intetech.ru>
2023-06-17 00:03:23 +0300

    ms-auth v7 cors

--- ms-auth/src/main/java/ru/profind/msauth/config/Config.java
+++ ms-auth/src/main/java/ru/profind/msauth/config/Config.java
@@ -3,13 +3,21 @@ package ru.profind.msauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 @Configuration
-public class Config
+public class Config implements WebMvcConfigurer
 {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder()
     {
